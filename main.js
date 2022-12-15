08410c8be1aafdd681be662c2e7b2c28d9de4b82
NikotinDev <118462078+NikotinDev@users.noreply.github.com>
2022-12-15 16:50:45 +0100

    Changed Page Design and setup Routing

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+const port = 8080;
 
+app.use(express.static('public'))
+
+app.get('/', (req,res) => {
+    res.sendFile('./public/html/main.html', {root: __dirname})
+})
+
+app.get('/category/gpu', (req,res) => {
+    res.sendFile('./public/html/category/gpu.html', {root: __dirname})
+})
+
+app.get('/category/cpu', (req,res) => {
+    res.sendFile('./public/html/category/cpu.html', {root: __dirname})
+})
+
+app.listen(port, () => {
+    console.log(`Listening in on port ${port}`)
+})
