7c75cadf702841632b205f8015d40816f147bef2
NikotinDev <118462078+NikotinDev@users.noreply.github.com>
2022-12-18 20:01:04 +0100

    Product pages (WIP)
    TO-DO: make things work!

--- app/readcsv.js
+++
@@ -1,9 +0,0 @@
-
-const readcsv = function(){
-
-    console.log("readcsv.js")
-
-}
-
-// export Module to use in other program
-module.exports = readcsv;
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // import express
 const express = require('express');
+const fs = require('fs')
 
 // setup express
 const app = express()
@@ -7,12 +8,43 @@ const app = express()
 // port
 const port = 8000;
 
+// prepare product pages
 const hostname = "localhost"
 
-// imports readcsv.js
-const readcsv = require('./app/readcsv')
 
-// setup static folder, so the user can access
+    // reads csv file
+    const csv = fs.readFileSync('./data/gpu.csv',"utf-8")
+
+    // splits csv data in rows
+    const products = csv.split('\n')
+    products.shift();
+    
+    let splitProducts = record => {
+
+        // split each data, when a ',' is found
+        const fields = record.split(',')
+        
+        // read html file
+        let html = fs.readFileSync('./public/html/product_template.html', "utf-8")
+        
+        html = html.replaceAll('{name}', fields[0])
+        html = html.replaceAll('{description}', fields[1])
+        html = html.replaceAll('{clock_speed}', fields[2])
+        html = html.replaceAll('{memory_type}', fields[3])
+        html = html.replaceAll('{memory_size}', fields[4])
+        html = html.replaceAll('{shading_units}', fields[5])
+        html = html.replaceAll('{amount}', fields[6])
+        html = html.replaceAll('{price}', fields[7])
+        html = html.replaceAll('{image_url}', fields[8])
+
+        return html;
+    }
+
+    const pages = products
+        .filter(row => row !== "")
+        .map(splitProducts);
+
+// setup static folder, so the users browser can access files within
 app.use(express.static('public'))
 
 app.get('/', (req,res) => {
@@ -27,10 +59,26 @@ app.get('/category/cpu', (req,res) => {
     res.sendFile('./public/html/category/cpu.html', {root: __dirname})
 })
 
+app.get('/category/gpu1', (req,res) => {
+    res.sendFile(pages[0])
+})
+
+app.get('/rx5600xt', (req,res) => {
+    res.sendFile('./public/images/rx5600xt.jpg', {root: __dirname})
+})
+
+app.get('/category/gpu2', (req,res) => {
+    res.sendFile(pages[1])
+})
+
+app.get('/rtx2060', (req,res) => {
+    res.sendFile('./public/images/rtx2060.jpg', {root: __dirname})
+})
+
 // if the url is not valid, the 404 page will be sent as a response 
 // '*' -> default case
 app.get('*', function(req,res){
-    res.sendFile('./public/html/404.html', {root: __dirname})
+    res.sendFile('./public/html/404.html')
 })
 
 app.listen(port,hostname, () => {
