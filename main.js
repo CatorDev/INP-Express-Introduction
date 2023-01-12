347a8d7d28efa3e07409ccb14422f45f5a205fe3
NikotinDev <118462078+NikotinDev@users.noreply.github.com>
2023-01-12 13:19:30 +0100

    Started Work on New Game Showcase

--- main.js
+++ main.js
@@ -11,39 +11,6 @@ const port = 8000;
 // prepare product pages
 const hostname = "localhost"
 
-
-    // reads csv file
-    const csv = fs.readFileSync('./data/gpu.csv',"utf-8")
-
-    // splits csv data in rows
-    const products = csv.split('\n')
-    products.shift();
-    
-    let splitProducts = record => {
-
-        // split each data, when a ',' is found
-        const fields = record.split(',')
-        
-        // read html file
-        let html = fs.readFileSync('./public/html/product_template.html', "utf-8")
-        
-        html = html.replaceAll('{name}', fields[0])
-        html = html.replaceAll('{description}', fields[1])
-        html = html.replaceAll('{clock_speed}', fields[2])
-        html = html.replaceAll('{memory_type}', fields[3])
-        html = html.replaceAll('{memory_size}', fields[4])
-        html = html.replaceAll('{shading_units}', fields[5])
-        html = html.replaceAll('{amount}', fields[6])
-        html = html.replaceAll('{price}', fields[7])
-        html = html.replaceAll('{image_url}', fields[8])
-
-        return html;
-    }
-
-    const pages = products
-        .filter(row => row !== "")
-        .map(splitProducts);
-
 // setup static folder, so the users browser can access files within
 app.use(express.static('public'))
 
